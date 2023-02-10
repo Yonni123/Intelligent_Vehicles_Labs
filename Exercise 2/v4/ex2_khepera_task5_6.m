2403f4956829fe1869aa54eb5ebb893dc1b8ef15
Yonni123 <86354472+Yonni123@users.noreply.github.com>
2023-02-10 21:46:21 +0100

    Fixed task1-4, I think?

--- Exercise 2/v4/ex2_khepera_task5_6.m
+++ Exercise 2/v4/ex2_khepera_task5_6.m
@@ -8,7 +8,8 @@ clear all;
 close all;
 
 % TASK SETTINGS
-SamplingRate = 10;   % Seldom of the sampling (1 uses all sample points, 10 uses every 10th point)
+CorrectionTerm = false; % weither or not to use correction term described in the wang paper
+SamplingRate = 1;   % Seldom of the sampling (1 uses all sample points, 10 uses every 10th point)
 
 % %%% Khepera settings 
 WHEEL_BASE = 53;                % [mm]
@@ -40,10 +41,6 @@ Y(1) = 0;
 A(1) = 90*pi/180;
 P(1,1:9) = [1 0 0 0 1 0 0 0 (1*pi/180)^2];
 
-% Save robot position without compensation term
-XW(1) = 0;
-YW(1) = 0;
-
 % Run until no more encoder values are available
 disp('Calculating ...');
 for kk=2:N,
@@ -51,50 +48,31 @@ for kk=2:N,
     dDr = Dr(kk) - Dr(kk-1);
     dDl = Dl(kk) - Dl(kk-1);
     
-    % Change of relative movements
-    dD = 0;
-    dA = 0.017;
-    dD = (dDr+dDl)/2;   % You should write the correct one, which replaces the one above!
-    dA = (dDr-dDl)/WHEEL_BASE;   % You should write the correct one, which replaces the one above!
-
-    % Save my dD and dA to an array so they can be plotted later
-    dDa(kk-1) = dD;
-    dAa(kk-1) = dA;
-    
     % Calculate the change in X and Y (World co-ordinates)
-    dX = 1;
-    dY = 1;
-    dX = dD * cos(A(kk-1)+(dA/2));   % You should write the correct one, which replaces the one above!
-    dY = dD * sin(A(kk-1)+(dA/2));   % You should write the correct one, which replaces the one above!
-    
-    % Check if we should use correction term or not
-    term = 1;
-    if dA ~= 0
-        term = sin(dA/2)/(dA/2);
-    end
+    dX = (dDr+dDl)/2 * cos(A(kk-1)+(dDr-dDl)/(2*WHEEL_BASE));   % You should write the correct one, which replaces the one above!
+    dY = (dDr+dDl)/2 * sin(A(kk-1)+(dDr-dDl)/(2*WHEEL_BASE));   % You should write the correct one, which replaces the one above!
+    dA = (dDr-dDl)/(WHEEL_BASE);
 
     % Predict the new state variables (World co-ordinates)
-    X(kk) = X(kk-1) + dX*term;
-    Y(kk) = Y(kk-1) + dY*term;
+    X(kk) = X(kk-1) + dX;
+    Y(kk) = Y(kk-1) + dY;
     A(kk) = mod(A(kk-1) + dA, 2*pi);
-
-    XW(kk) = X(kk-1) + dX;
-    YW(kk) = Y(kk-1) + dY;
     
     % Predict the new uncertainty in the state variables (Error prediction)
     Cxya_old = [P(kk-1,1:3);P(kk-1,4:6);P(kk-1,7:9)];   % Uncertainty in state variables at time k-1 [3x3]   
-    a = mod(A(kk-1) + dA/2, 2*pi);  % The angle T + dT/2
+    a = mod(A(kk-1), 2*pi);  % The angle T + dT/2
 
     Cu =   [1 0;0 1];               % Uncertainty in the input variables [2x2]
     Axya = [1 0 0;0 1 0;0 0 1];     % Jacobian matrix w.r.t. X, Y and A [3x3]
     Au =   [0 0;0 0;0 0];           % Jacobian matrix w.r.t. dD and dA [3x2]
 
-    Axya = [1   0   -dD*sin(a);
-            0   1   dD*cos(a);
+    
+    Axya = [1   0   sin(a);
+            0   1   cos(a);
             0   0       1]; % You should write the correct one, which replaces the one above!
 
-    Au =   [cos(a)  (-dD/2)*sin(a);
-            sin(a)  (dD/2)*cos(a);
+    Au =   [cos(a)  sin(a);
+            sin(a)  cos(a);
                 0       1];   % You should write the correct one, which repleces the one above!
 
     Cu =   [(SIGMAr^2+SIGMAl^2)/4   0;
@@ -111,7 +89,6 @@ for kk=2:N,
     plot_khepera([X(kk);Y(kk);A(kk)], WHEEL_DIAMETER, WHEEL_BASE, 3);
     drawnow();
 end;
-CV = cov(dDa,dAa*180/pi);
 
 disp('Plotting ...');
 
@@ -131,24 +108,9 @@ figure;
 
 % After the run, plot the results (X,Y,A), i.e. the estimated positions 
 figure;
-
-subplot(3,1,1);
-plot(X, 'b');
-hold on;
-plot(XW, 'r');
-title('X [mm]');
-legend('X', 'XW');
-
-subplot(3,1,2);
-plot(Y, 'b');
-hold on;
-plot(YW, 'r');
-title('Y [mm]');
-legend('Y', 'YW');
-
-subplot(3,1,3);
-plot(A*180/pi, 'b');
-title('A [deg]');
+    subplot(3,1,1); plot(X, 'b'); title('X [mm]');
+    subplot(3,1,2); plot(Y, 'b'); title('Y [mm]');
+    subplot(3,1,3); plot(A*180/pi, 'b'); title('A [deg]');
 
 % Plot the estimated variances (in X, Y and A) - 1 standard deviation
 subplot(3,1,1); hold on;
@@ -168,8 +130,3 @@ hold off;
 figure;
     subplot(2,1,1); plot(dDa, 'b'); title('dD');
     subplot(2,1,2); plot(dAa*180/pi, 'b'); title('dA [deg]');
-
-
-figure;
-    diff = X-XW;
-    plot(diff, 'b');
